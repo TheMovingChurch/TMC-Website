724670a8eb70245ac423d9047b0fad52d0a723a2
KweonMo Koo <mikekoo2010@gmail.com>
2022-05-02 02:03:22 -0700

    Lint files

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,22 +1,22 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '@/components/container'
-import PostBody from '@/components/post-body'
-import MoreStories from '@/components/more-stories'
-import Header from '@/components/header'
-import PostHeader from '@/components/post-header'
-import SectionSeparator from '@/components/section-separator'
-import Layout from '@/components/layout'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '@/lib/api'
-import PostTitle from '@/components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '@/lib/constants'
-import RichTextResolver from 'storyblok-js-client/dist/richTextResolver'
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import Head from 'next/head';
+import RichTextResolver from 'storyblok-js-client/dist/richTextResolver';
+import Container from '@/components/container';
+import PostBody from '@/components/post-body';
+import MoreStories from '@/components/more-stories';
+import Header from '@/components/header';
+import PostHeader from '@/components/post-header';
+import SectionSeparator from '@/components/section-separator';
+import Layout from '@/components/layout';
+import { getAllPostsWithSlug, getPostAndMorePosts } from '@/lib/api';
+import PostTitle from '@/components/post-title';
+import { CMS_NAME } from '@/lib/constants';
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
+  const router = useRouter();
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <Layout preview={preview}>
@@ -29,7 +29,10 @@ export default function Post({ post, morePosts, preview }) {
             <article>
               <Head>
                 <title>
-                  {post.content.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.content.title}
+                  {' '}
+                  | Next.js Blog Example with
+                  {CMS_NAME}
                 </title>
                 <meta property="og:image" content={post.content.image} />
               </Head>
@@ -47,11 +50,11 @@ export default function Post({ post, morePosts, preview }) {
         )}
       </Container>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps({ params, preview = null }) {
-  const data = await getPostAndMorePosts(params.slug, preview)
+  const data = await getPostAndMorePosts(params.slug, preview);
 
   return {
     props: {
@@ -64,13 +67,13 @@ export async function getStaticProps({ params, preview = null }) {
       },
       morePosts: data.morePosts,
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug()
+  const allPosts = await getAllPostsWithSlug();
   return {
     paths: allPosts?.map((post) => `/posts/${post.slug}`) || [],
     fallback: true,
-  }
+  };
 }
