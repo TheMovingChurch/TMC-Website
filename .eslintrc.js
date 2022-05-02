936ab3dc0bfe8bde9d14b76b530f0898e7f2eec2
KweonMo Koo <mikekoo2010@gmail.com>
2022-05-02 02:30:13 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,39 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: 'airbnb',
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
   },
+  plugins: ["react", "prettier"],
   rules: {
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'import/extensions': 0,
+    'import/no-unresolved': 'warn',
     'react/function-component-definition': [
-      'warn',
+      0,
       { namedComponents: 'arrow-function' },
     ],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/react-in-jsx-scope': 'warn',
+    'react/react-in-jsx-scope': 0,
     'react/prop-types': 'warn',
-    'import/extensions': 0,
-    // "prettier/prettier": "warn"
+    // 'prettier/prettier': 'warn'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['.'],
+      },
+      alias: {
+        map: [
+          ['@/*', '/*'],
+          // ['@/styles', './styles'],
+          // ['@/components', '/components'],
+          // Add more here
+        ],
+        extensions: ['.js', '.jsx'],
+      },
+    },
   },
 };
