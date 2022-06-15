9b0172d6443f98e792042bcc4101a045097a0abb
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-14 23:12:56 -0700

    Clean up

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import MoreStories from '@/components/more-stories'
 import HeroPost from '@/components/hero-post'
 import Intro from '@/components/intro'
 import Layout from '@/components/layout'
-import { getAllPostsForHome } from '@/lib/api'
+// import { getAllPostsForHome } from '@/lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
 
+import { getStoryblokApi } from "@storyblok/react"
+
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  // const heroPost = allPosts[0]
+  // const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Home | The Moving Church</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
+          {/* {heroPost && (
             <HeroPost
               title={heroPost.content.title}
               coverImage={heroPost.content.image}
@@ -26,18 +28,18 @@ export default function Index({ allPosts, preview }) {
               author={heroPost.content.author}
               slug={heroPost.slug}
               excerpt={heroPost.content.intro}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            /> */}
+          {/* )} */}
+          {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
         </Container>
       </Layout>
     </>
   )
 }
 
-export async function getStaticProps({ preview = null }) {
-  const allPosts = (await getAllPostsForHome(preview)) || []
-  return {
-    props: { allPosts, preview },
-  }
-}
+// export async function getStaticProps({ preview = null }) {
+//   const allPosts = (await getAllPostsForHome(preview)) || []
+//   return {
+//     props: { allPosts, preview },
+//   }
+// }
