c429d544a947ef45982d6ca79e92b3bdd95a8c5e
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-14 23:11:56 -0700

    Initialize storyblok

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/index.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: process.env.STORYBLOK_PREVIEW_SECRET,
+  use: [apiPlugin]
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
